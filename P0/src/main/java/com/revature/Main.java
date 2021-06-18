010680648d7abc5017a69599d71fb94a8b239b1f
Mark Ratib <mark.ratib@revature.net>
2021-06-18 13:32:59 -0700

    Made some preliminary steps

---
+++ P0/src/main/java/com/revature/Main.java
@@ -0,0 +1,17 @@
+package com.revature;
+
+import com.revature.models.*;
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		//create Menu object
+		Menu poorMenu = new Menu();
+		//Show the Main Menu
+		poorMenu.showMainMenu();
+
+	}
+
+}
---
+++ P0/src/main/java/com/revature/models/Menu.java
@@ -0,0 +1,10 @@
+package com.revature.models;
+
+public class Menu 
+{
+	public void showMainMenu()
+	{
+		System.out.println("========MAIN=MENU========");
+		
+	}
+}
